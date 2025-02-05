004c07fcf60c50b987fb6ffbd2ee9dffdd9fc313
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2025-02-05 10:22:04 +0100

    Fix Copilot setup

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,21 @@
 return {
-	"zbirenbaum/copilot.lua"
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	build = ":Copilot auth",
+	event = "InsertEnter",
+	opts = {
+		panel = {
+			enabled = true
+		},
+		suggestion = {
+			auto_trigger = true,
+			keymap = {
+				accept = "<C-Enter>"
+			}
+		},
+		filetypes = {
+			help = false,
+			markdown = false
+		},
+	}
 }
